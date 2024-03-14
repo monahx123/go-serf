4fb3009b130e40b0759291a0238a6a08caeeed4d
monahx123 <dimabilogrivcev@gmail.com>
2024-03-14 23:35:49 +0200

    go-serf

--- app/js/main.js
+++ app/js/main.js
@@ -13,4 +13,11 @@ $(function(){
     asNavFor: '.header__slider',
   });
 
+  $('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
+  });
+
 });
